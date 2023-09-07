a41d399a500a7f3feea4e3918e9d5f3da68df666
XRhahelry <sujalbajracharya1@gmail.com>
2023-09-07 19:01:57 +0545

    Modeline file size and state indicator removed

--- config.el
+++ config.el
@@ -1,7 +1,7 @@
 (setq doom-theme 'doom-one)
 
 (setq doom-font (font-spec :family "JetBrains Mono" :size 20 :weight 'light)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 15 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "Iosevka" :size 15 :weight 'light)
       doom-big-font (font-spec :family "JetBrains Mono" :size 24))
 
 (after! doom-themes
@@ -24,19 +24,24 @@
 (setq-default tab-width 2)
 (setq-default evil-shift-width tab-width)
 
+;; (define-key evil-normal-state-map (kbd "^") (kbd "H"))
+;; (define-key evil-normal-state-map (kbd "$") (kbd "L"))
+;; (define-key evil-visual-state-map (kbd "^") (kbd "H"))
+;; (define-key evil-visual-state-map (kbd "$") (kbd "L"))
+
 (map! :leader
       :desc "Org babel tangle" "m B" #'org-babel-tangle)
 (after! org
   (setq inhibit-compacting-font-caches t)
   (custom-set-faces
-    '(org-level-1 ((t (:inherit outline-1 :height 1.7 :weight 'ultra-bold))))
-    '(org-level-2 ((t (:inherit outline-2 :height 1.6 :weight 'extra-bold))))
-    '(org-level-3 ((t (:inherit outline-3 :height 1.5 :weight 'bold))))
-    '(org-level-4 ((t (:inherit outline-4 :height 1.4 :weight 'semi-bold))))
-    '(org-level-5 ((t (:inherit outline-5 :height 1.3 :weight 'normal))))
-    '(org-level-6 ((t (:inherit outline-6 :height 1.2 :weight 'normal))))
-    '(org-level-7 ((t (:inherit outline-7 :height 1.1 :weight 'normal))))
-    '(org-level-8 ((t (:inherit outline-8 :height 1.0 :weight 'normal)))))
+    '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
+    '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
+    '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
+    '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+    '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
+    '(org-level-6 ((t (:inherit outline-6 :height 1.2))))
+    '(org-level-7 ((t (:inherit outline-7 :height 1.1))))
+    '(org-level-8 ((t (:inherit outline-8 :height 1.0)))))
   (setq org-directory "c:/Users/N I T R O 5/Documents/Org/"
         org-default-notes-file (expand-file-name "notes.org" org-directory)
         org-ellipsis " ▼ "
@@ -52,10 +57,24 @@
       :desc "Comment or uncomment lines"      "TAB TAB" #'comment-line)
 
 (set-face-attribute 'mode-line nil :font "Cascadia Code-15")
-(setq doom-modeline-height 50     ;; sets modeline height
+(setq doom-modeline-height 40     ;; sets modeline height
       doom-modeline-bar-width 5   ;; sets right bar width
       doom-modeline-persp-name t  ;; adds perspective name to modeline
-      doom-modeline-persp-icon t) ;; adds folder icon next to persp name
+      doom-modeline-persp-icon t  ;; adds folder icon next to persp name when set to t
+      doom-modeline-buffer-file-name-style 'truncate-except-project ;; almost full path to file
+      doom-modeline-buffer-modification-icon nil ;; removes icon next to file path when file is changed
+      doom-modeline-project-detection 'project ;; does something when working in a project
+      doom-modeline-major-mode-icon nil
+      doom-modeline-highlight-modified-buffer-name nil
+      doom-modeline-modal nil
+      doom-modeline-modal-icon nil
+      doom-modeline-battery t
+      doom-modeline-time t)
+(after! doom-modeline
+  (remove-hook 'doom-modeline-mode-hook #'size-indication-mode) ; filesize in modeline
+  (remove-hook 'doom-modeline-mode-hook #'column-number-mode)   ; cursor column in modeline
+  (line-number-mode -1)
+  (setq doom-modeline-buffer-encoding nil))
 
 (xterm-mouse-mode 1)
 
@@ -63,5 +82,3 @@
   (set-variable 'split-height-threshold nil t)
   (set-variable 'split-width-threshold 40 t)) ; make this as low as needed
 (add-hook 'markdown-mode-hook 'prefer-horizontal-split)
-(map! :leader
-      :desc "Clone indirect buffer other window" "b c" #'clone-indirect-buffer-other-window)
--- packages.el
+++ packages.el
@@ -3,7 +3,6 @@
 
 (package! powershell)
 (package! gruber-darker-theme)
-(package! latex-preview-pane)
 (package! org-auto-tangle)
 
 ;; To install a package directly from a remote git repo, you must specify a
